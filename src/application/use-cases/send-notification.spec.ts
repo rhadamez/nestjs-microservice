d0a05ccb36b4063bef9c5b98ff2da8bb193c2502
Rhadamez Gindri Hercilio <rhadamez@gmail.com>
2022-12-15 01:45:20 -0300

    feat: add InMemoryNotificationRepository

---
+++ src/application/use-cases/send-notification.spec.ts
@@ -0,0 +1,23 @@
+import { InMemoryNotificationsRepository } from '../../../test/repositories/in-memory-notifications-repository';
+import { SendNotification } from './send-notification';
+
+describe('Send notification', () => {
+  let sendNotification: SendNotification;
+  let notificationsRepository: InMemoryNotificationsRepository;
+
+  beforeEach(() => {
+    notificationsRepository = new InMemoryNotificationsRepository();
+    sendNotification = new SendNotification(notificationsRepository);
+  });
+
+  test('it should be able to send a notification', async () => {
+    const { notification } = await sendNotification.execute({
+      content: 'Nova solicitacao de amizade',
+      category: 'social',
+      recipientId: 'exemple-recipient-id',
+    });
+
+    expect(notificationsRepository.notifications).toHaveLength(1);
+    expect(notificationsRepository.notifications[0]).toEqual(notification);
+  });
+});
